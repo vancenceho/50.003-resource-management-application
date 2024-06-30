b32f006353311ef33702e2c91176b36af5c0c572
Vancence Ho <vancenceho@gmail.com>
2024-07-01 01:20:43 +0800

    feat: added react.js framework for front-end development

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.use(
+  express.static(
+    path.join(
+      __dirname,
+      "../frontend-service/react-res-management-app",
+      "public"
+    )
+  )
+);
+
+app.get("*", function (req, res) {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "../frontend-service/react-res-management-app",
+      "public",
+      "index.html"
+    )
+  );
 });
 
-module.exports = router;
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+
+// module.exports = router;
