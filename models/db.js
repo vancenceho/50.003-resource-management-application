f3b67c5d3f37ddf934a43e5193b5016721f1ade9
Vancence Ho <vancenceho@gmail.com>
2024-07-05 00:59:35 +0800

    feat: CRUD functions for workshop

--- models/db.js
+++ models/db.js
@@ -1,20 +1,27 @@
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 
 const connection_str = "mongodb://root:example@localhost:27017/";
 
-const client = new MongoClient(connection_str);
+// const client = new MongoClient(connection_str);
 const dbName = "dell-mongodb";
+const source = "?authSource=admin";
 
 var db = null;
 
-try {
-  db = client.db(dbName);
-} catch (error) {
-  console.error("database connection failed. " + error);
-}
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(connection_str + dbName + source);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB Connection Failed: ", err);
+    process.exit(1);
+  }
+};
 
 async function cleanup() {
-  await client.disconnect();
+  await mongoose.disconnect();
 }
 
-module.exports = { db, cleanup };
+module.exports = { connectDB, cleanup };
