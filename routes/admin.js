987ced97faeeaf7d0a3ad12fdfdb34aa2dd0f3d1
Vancence Ho <vancenceho@gmail.com>
2024-07-08 01:41:07 +0800

    docs: added documentation for admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,49 @@ const router = express.Router();
 const workshopController = require("../controller/workshopManagement");
 const adminController = require("../controller/adminController");
 
-// Middleware functions
+// Middleware Controller
 const { authenticateUser, authorizeRole } = require("../middleware/auth");
 
-// Example routes with authentication and authorization
+/**
+ * // Admin Default Routes
+ *
+ * @details
+ * Route 1: Route to create a new admin
+ * Route 2: Route to login as an admin
+ * Route 3: Route to get all admins
+ *
+ * @returns
+ * Route 1: Returns a 200 status code with the new admin data.
+ * Route 2: Returns a 200 status code with the admin data.
+ * Route 3: Returns a 200 status code with all the admin data.
+ *
+ * Route 1 - 3: Returns a 500 status code with an error message if there is an error.
+ *
+ */
 router.post("/createadmin", adminController.createAdmin);
 router.get("/adminlogin", adminController.Adminlogin);
 router.get("/getadmin", adminController.getAllAdmin);
 
+/**
+ * // Admin Workshop Request Routes
+ *
+ * @details
+ * Route 1: Route to get all workshop requests
+ * Route 2: Route to get a specific workshop request by id
+ * Route 3: Route to create a new workshop request
+ * Route 4: Route to update a workshop request
+ * Route 5: Route to delete a workshop request
+ *
+ * @returns
+ * Route 1: Returns a 200 status code with the workshop requests data.
+ * Route 2: Returns a 200 status code with the workshop request data.
+ * Route 3: Returns a 200 status code with the new workshop request data.
+ * Route 4: Returns a 200 status code with the updated workshop request data.
+ * Route 5: Returns a 200 status code with the deleted workshop request data.
+ *
+ * Route 1 - 5: Returns a 500 status code with an error message if there is an error.
+ *
+ */
 router.get(
   "/",
   authenticateUser,
