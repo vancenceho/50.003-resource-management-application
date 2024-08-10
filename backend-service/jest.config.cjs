536b4e278ea58e10276ba13a110560c75f45312b
rouzhen <koorouzhen@gmail.com>
2024-08-10 11:47:19 +0800

    latest version

--- backend-service/jest.config.cjs
+++ backend-service/jest.config.cjs
@@ -10,8 +10,10 @@ const config = {
     moduleFileExtensions: ["js", "json", "ts", "tsx"],
     moduleNameMapper: {
       "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$": "jest-transform-stub"
+      "\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$": "jest-transform-stub"
     },
+    collectCoverage: true,
+    coverageReporters: ["text", "html"]
 };
 
 module.exports = config;
